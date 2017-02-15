a7b7f908e789a21f31a8112ef16a3cf00f82834a
arnaudbarre <arnaudbarremn@gmail.com>
2017-02-15 01:09:59 +0100

    Improve list files display

--- app/src/main/java/com/emn/trustydrive/adapters/FileAdapter.java
+++ app/src/main/java/com/emn/trustydrive/adapters/FileAdapter.java
@@ -5,9 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.emn.trustydrive.R;
@@ -46,22 +44,22 @@ public class FileAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        convertView = inflater.inflate(R.layout.view_item_file, parent, false);
-        TextView nameTextView = (TextView) convertView.findViewById(R.id.nameTextView);
-        TextView sizeAndDateTextView = (TextView) convertView.findViewById(R.id.sizeAndDateTextView);
-        LinearLayout documentInfo = (LinearLayout) convertView.findViewById(R.id.documentInfo);
-        ImageButton fileOptionsButton = (ImageButton) convertView.findViewById(R.id.fileOptionsButton);
-        ImageView imageIcon = (ImageView) convertView.findViewById(R.id.imageIcon);
-        ImageView storedStatusImage = (ImageView) convertView.findViewById(R.id.storedStatusImage);
         FileData fileData = files.get(position);
-        nameTextView.setText(fileData.getName());
-        if (fileData.getType() == Type.FILE)
-            sizeAndDateTextView.setText(fileData.displaySize() + ", " + fileData.displayDate());
-        documentInfo.setTag(position);
-        fileOptionsButton.setTag(position);
-        storedStatusImage.setImageResource(fileData.isOnDevice() ? R.mipmap.on_device_icon : 0);
-        if (fileData.getType() == Type.DIRECTORY)
-            imageIcon.setImageResource(R.drawable.folder_icon);
+        if (fileData.getType() == Type.FILE) {
+            convertView = inflater.inflate(R.layout.view_item_file, parent, false);
+            ((TextView) convertView.findViewById(R.id.nameTextView)).setText(fileData.getName());
+            ((TextView) convertView.findViewById(R.id.sizeAndDateTextView))
+                    .setText(fileData.displaySize() + ", " + fileData.displayDate());
+            convertView.findViewById(R.id.documentInfo).setTag(position);
+            convertView.findViewById(R.id.fileOptionsButton).setTag(position);
+            ((ImageView) convertView.findViewById(R.id.storedStatusImage))
+                    .setImageResource(fileData.isOnDevice() ? R.mipmap.on_device_icon : 0);
+        } else {
+            convertView = inflater.inflate(R.layout.view_item_folder, parent, false);
+            ((TextView) convertView.findViewById(R.id.name)).setText(fileData.getName());
+            convertView.findViewById(R.id.name).setTag(position);
+            convertView.findViewById(R.id.options).setTag(position);
+        }
         return convertView;
     }
 }
